8c332504da88dd668c41e35be7e62b4b883d805d
Ikchhit Kumar Pandey <89184573+wizeewig@users.noreply.github.com>
2022-08-07 01:15:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var randomNumber1 = Math.floor(Math.random()*6)+1;
 
-var randomDiceImage1 = "/images/dice"+randomNumber1+".png";
+var randomDiceImage1 = "/images/dice"+"1"+".png";
 
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src",randomDiceImage1);
